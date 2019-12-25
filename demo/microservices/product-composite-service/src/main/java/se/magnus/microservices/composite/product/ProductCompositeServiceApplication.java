8e05bc3e646430872c02f45ac853592363adda00
Hoai Duc Nguyen <hoaius7@gmail.com>
2019-12-24 16:20:58 -0800

    spring data

--- demo/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ demo/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,28 +1,27 @@
 package se.magnus.microservices.composite.product;
 
-import static java.util.Collections.emptyList;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
-import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
+import static java.util.Collections.emptyList;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
 @EnableSwagger2WebFlux
 @SpringBootApplication
 @ComponentScan("se.magnus")
 public class ProductCompositeServiceApplication {
-	
+
     @Value("${api.common.version}")           String apiVersion;
     @Value("${api.common.title}")             String apiTitle;
     @Value("${api.common.description}")       String apiDescription;
@@ -46,7 +45,9 @@ public class ProductCompositeServiceApplication {
 			.apis(basePackage("se.magnus.microservices.composite.product"))
 			.paths(PathSelectors.any())
 			.build()
-                .globalResponseMessage(GET, emptyList())
+				.globalResponseMessage(POST, emptyList())
+				.globalResponseMessage(GET, emptyList())
+				.globalResponseMessage(DELETE, emptyList())
 				.apiInfo(new ApiInfo(
                     apiTitle,
                     apiDescription,
@@ -58,7 +59,7 @@ public class ProductCompositeServiceApplication {
                     emptyList()
                 ));
     }
-	
+
 	@Bean
 	RestTemplate restTemplate() {
 		return new RestTemplate();
--- demo/util/src/main/java/se/magnus/util/exceptions/InvalidInputException.java
+++ demo/util/src/main/java/se/magnus/util/exceptions/InvalidInputException.java
@@ -1,20 +1,18 @@
 package se.magnus.util.exceptions;
 
 public class InvalidInputException extends RuntimeException {
-	private static final long serialVersionUID = 2639687325711549213L;
+    public InvalidInputException() {
+    }
 
-	public InvalidInputException() {
-	}
+    public InvalidInputException(String message) {
+        super(message);
+    }
 
-	public InvalidInputException(String message) {
-		super(message);
-	}
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-	public InvalidInputException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public InvalidInputException(Throwable cause) {
-		super(cause);
-	}
+    public InvalidInputException(Throwable cause) {
+        super(cause);
+    }
 }
